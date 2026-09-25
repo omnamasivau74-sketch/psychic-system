14f28e48e3d19739835f3f2cc2b7c228fc875243
omnamasivau74-sketch <omnamasivau74@gmail.com>
2026-09-25 14:34:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,22 @@
-from flask import Flask
-from threading import Thread
 import logging
 import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 import yt_dlp
 
-app = Flask(__name__)
-@app.route('/')
-def home():
-    return "Bot is Alive!"
-
-def run_web():
-    app.run(host='0.0.0.0', port=10000)
-
-Thread(target=run_web).start()
-
 logging.basicConfig(level=logging.INFO)
 TOKEN = os.environ.get("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Link bhej, download karta hu!")
+    await update.message.reply_text("Link bhej, download karta hu 👇")
 
 async def download_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     url = update.message.text
     if "http" not in url:
         return
-    msg = await update.message.reply_text("Downloading...")
+    msg = await update.message.reply_text("Downloading... ⏳")
     try:
-        ydl_opts = {'format': 'best', 'outtmpl': '%(title)s.%(ext)s', 'extractor_args': {'youtube': {'player_client': ['android']}}, 'noplaylist': True}
+        ydl_opts = {'format': 'best', 'outtmpl': '%(title)s.%(ext)s', 'noplaylist': True}
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
             filename = ydl.prepare_filename(info)
